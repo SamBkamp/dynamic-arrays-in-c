1db89129fabad697d24881ed4fd9d38b1f27d31b
Sam Bonnekamp <sam@bonnekamp.net>
2025-08-26 04:31:33 +0800

    updated example usage

--- main.c
+++ main.c
@@ -5,12 +5,19 @@
 int main(int argc, char* argv[]){
   
   Array a = arrayInit();
+  //push to array
   pushArray(&a,3);
   pushArray(&a,4);
-  pushArray(&a,7);
-  pushArray(&a,9);
   pushArray(&a,10);
+  
+  //access random values of array
+  for (int i = 0; i < a.used; i++){
+    printf("%d\n", a.values[i]);
+  }
+  
+  //pop last value of array
   printf("POPPED: %d\n", popArray(&a));
-  printArray(&a);
+
+  //free array
   free(a.values);
 }
